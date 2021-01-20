e5f01fcd41b88fb140807aa92aaa192bbbfc1a44
gurb <herbokvar@outlook.com>
2021-01-20 22:06:16 +0300

    added node class

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
+import math
+
 map_data = [
     # 0    1    2    3    4
-    [0x0, 0x0, 0x0, 0x0, 0x0], # 0
+    [0x1, 0x0, 0x0, 0x0, 0x0], # 0
     [0x0, 0x0, 0xb, 0x0, 0x0], # 1
     [0x0, 0x0, 0xb, 0x0, 0x0], # 2
     [0x0, 0x0, 0x0, 0x0, 0x2], # 3
-    [0x0, 0x0, 0x0, 0x0, 0x1]  # 4
+    [0x0, 0x0, 0x0, 0x0, 0x0]  # 4
 ]
+
+class Node:
+    def __init__(self, pos):
+        self.pos = pos
+        self.data = data
+        self.H = 0
+        self.G = 0
+        self.F = 0
+
+    def calc_distance(self, pos1, pos2):
+        # euclidean distance
+        return math.sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)
+
+    def set_h(self, target):
+        self.H = self.calc_distance(target, self.pos)
+    
+    def set_g(self, current)
+        self.G = self.calc_distance(current, self.pos)
+    
+    def set_f(self):
+        self.F = self.H + self.G
+    
+    def get_h(self):
+        return self.H
+    
+    def get_g(self):
+        return self.G
+    
+    def get_f(self):
+        return self.F
+    
 
 dirs = [(1,0), (0,1), (-1,0), (0,-1), (1,1), (-1,-1), (1,-1), (-1,1)]
 
-start = (4,4)
+start = (0,0)
 end = (3,4)
 
 def sum_coords(a,b):
